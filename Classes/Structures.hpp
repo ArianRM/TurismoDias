54f77024b140b03253c7ecdf9459d06b3d04abbc
DiegoS-R <u202021367@upc.edu.pe>
2022-11-20 11:57:54 -0500

    Feat: Se añade lista doble a Structures.hpp

--- Classes/Structures.hpp
+++ Classes/Structures.hpp
@@ -86,251 +86,88 @@ public:
 };
 
 
-//#####################// Simple List //#########################//
+//#####################// Double List //#########################//
 
 template <typename G>
 struct ListNode
 {
 	G data;
 	ListNode* next;
+	ListNode* prev;
 	size_t factor;
 
-	Node(G data = nullptr, ListNode* next = nullptr, size_t factor = 0) : data(data), next(next), factor(factor) {}
+	ListNode(G data = nullptr, ListNode* next = nullptr, ListNode* prev = nullptr, size_t factor = 0) : 
+		data(data), next(next), factor(factor) {}
 };
 
-template <class G, G BLANK>
-class List {
-    
-    typedef function<int(G, G)> Compare;
+template <class G>
 
-    ListNode* first;
-    ListNode* last;
-    ListNode* tmp;
-    size_t size; // número de elementos en la lista
-
-    Compare compare; // lambda de criterio de comparaci�n
+class DLL {
+	ListNode<G>* begin;
+	ListNode<G>* end;
+	long size;
 
 public:
-    List() : first(nullptr), last(nullptr), size(0), compare([](G a, G b) {return a - b; }) {}
-    List(Compare compare) : first(nullptr), last(nullptr), size(0), compare(compare) {}
-    ~List();
+	DLL() { begin = nullptr; end = nullptr; size = 0; }
 
-    // Retorno de la longitud de lista
-    size_t getSize();
+	// Insertar al inicio y al final
+	void push_front(G element)
+	{
+		ListNode<G>* aux = new ListNode<G>(element);
 
-    // Insertar elemento en la lista
-    void    push_front(G elem);
-    void    addPos(G elem, size_t pos);
-    void    push_back(G element);
+		if (size == 0)
+			begin = end = aux;
+		else
+		{
+			aux->next = begin;
+			begin->prev = aux;
+			begin = aux;
+		}
+		++size;
+	}
+	void push_back(G element) {
+		ListNode<G>* aux = new ListNode<G>(element);
+		if (size == 0)
+			begin = end = aux;
+		else {
+			end->next = aux;
+			aux->prev = end;
+			end = aux;
+		}
+		++size;
+	}
 
-    //Borrar elemento de la lista
-    void delete_back();
-    void delete_at(size_t pos);
-    void delete_front();
+	long getSize() { return size; }
 
-    //Obtener elemento de la lista
-    G       getFirst();
-    G       getPos(size_t pos);
-    G       getLast();
+	// Obtener un valor del nodo
+	G& operator[](const int& pos) {
+		ListNode<G>* aux = begin;
+		for (int i = 0; i < pos; i++) {
+			aux = aux->next;
+		}
+		return aux->element;
+	}
 
-    // Por terminar
-    void    modifyFirst(G elem);
-    void    modifyPos(G elem, size_t pos);
-    void    modifyLast(G elem);
-    G       search(G elem);
-    //void print(function<void(G)> print);
+	void selectionSort() {}
+	void insertionSort() {}
+
+	// Mostrar el valor del nodo
+	void showElement() {
+		ListNode<G>* aux = begin;
+		for (size_t i = 0; i < size; ++i)
+		{
+			cout << aux->element << " ";
+			aux = aux->next;
+			if (aux->element == nullptr)
+			{
+				cout << " Valor no encontrado :( ";
+			}
+		}
+		cout << "\n";
+	}
+
 };
 
-
-
-template <typename G, G BLANK>
-List<G, BLANK>::~List() {
-    Node* aux = first;
-    while (aux != nullptr) {
-        aux = first;
-        first = first->next;
-        delete aux;
-    }
-}
-
-template <typename G, G BLANK>
-size_t List<G, BLANK>::getSize() { return size; }
-
-
-//Insertar
-template <typename G, G BLANK>
-void List<G, BLANK>::push_front(G elem) {
-    Node* aux = new Node(elem);
-    if (size == 0)
-        first = last = aux;
-    else {
-        aux->next = first;
-        aux->next->prev = aux;
-        first = aux;
-    }
-    ++size;
-}
-
-template <typename G, G BLANK>
-void List<G, BLANK>::push_back(G elem) {
-    Node* news = new Node(elem);
-    if (size == 0)
-        first = last = news;
-    else {
-        last->next = news;
-        news->prev = last;
-        last = news;
-    }
-    ++size;
-}
-
-template <typename G, G BLANK>
-void List<G, BLANK>::addPos(G elem, size_t pos) {
-    if (pos<0 || pos>size)return;
-    if (pos == 0)
-        push_front(elem);
-    else if (pos == size)
-        push_back(elem);
-    else {
-        Node* news = new Node(elem);
-        Node* aux = first;
-        for (size_t i = 0; i < pos - 1; ++i) {
-            aux = aux->next;
-        }
-        news->next = aux->next;
-        news->prev = aux;
-        news->next->prev = news;
-        aux->next = news;
-        ++size;
-    }
-}
-
-
-//Eliminar
-template <typename G, G BLANK>
-void List<G, BLANK>::delete_front() {
-    if (size == 0)return;
-    else if (size == 1) {
-        delete first;
-        first = last = nullptr;
-    }
-    else {
-        Node* aux = first;
-        first = first->next;
-        delete aux;
-        first->prev = nullptr;
-    }
-    --size;
-}
-
-template <typename G, G BLANK>
-void List<G, BLANK>::delete_back() {
-    if (size == 0)return;
-    else if (size == 1)
-    {
-        delete last;
-        first = last = nullptr;
-        --size;
-    }
-    else {
-        last = last->prev;
-        delete last->next;
-        last->next = nullptr;
-        --size;
-    }
-}
-
-template <typename G, G BLANK>
-void List<G, BLANK>::delete_at(size_t pos) {
-    if (pos == 0) { delete_front(); }
-    else if (pos == size - 1) { delete_back(); }
-    else {
-        Node* aux = first;
-        for (int i = 0; i < pos; i++) {
-            aux = aux->next;
-        }
-        Node* obj = aux->next;
-        aux->prev->next = obj;
-        obj->prev = aux->prev->next;
-        size--;
-    }
-}
-
-
-//Obtener
-template <typename G, G BLANK>
-G List<G, BLANK>::getFirst() {
-    return getPos(0);
-}
-template <typename G, G BLANK>
-G List<G, BLANK>::getPos(size_t pos) {
-    if (pos >= 0 && pos <= size) {
-        Node* aux = first;
-        for (int i = 0; i < pos; ++i) {
-            aux = aux->next;
-        }
-        return aux->elem;
-    }
-    else {
-        return BLANK;
-    }
-}
-template <typename G, G BLANK>
-G List<G, BLANK>::getLast() {
-    return getPos(size - 1);
-}
-
-
-//Por terminar
-template <typename G, G BLANK>
-void List<G, BLANK>::modifyFirst(G elem) {
-    if (size > 0) {
-        first->elem = last->elem = elem;
-    }
-}
-
-template <typename G, G BLANK>
-void List<G, BLANK>::modifyPos(G elem, size_t pos) {
-    if (pos >= 0 && pos < size) {
-        Node* aux = first;
-        for (int i = 0; i < pos; i++) {
-            aux = aux->next;
-        }
-        aux->elem = elem;
-    }
-}
-
-template <typename G, G BLANK>
-void List<G, BLANK>::modifyLast(G elem) {
-    modifyPos(elem, size - 1);
-}
-
-template <typename G, G BLANK>
-G List<G, BLANK>::search(G elem) {
-    Node* aux = first;
-    while (aux != nullptr) {
-        if (compare(aux->elem, elem) == 0) {
-            cout << "Se encontro el valor buscado";
-            return aux->elem;
-        }
-
-
-        aux = aux->next;
-    }
-    cout << "No se encontro";
-
-    return BLANK;
-}
-/*
-template <typename G, G BLANK>
-void List<G, BLANK>::print(function<void(G)> imprime) {
-    Node<G>* aux = first;
-    while (aux != nullptr) {
-        imprime(aux->elem);
-        aux = aux->next;
-    }
-    cout << "\n";
-}*/
 
 
 
